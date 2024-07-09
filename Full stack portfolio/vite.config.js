9d3f34aee0e9c5d71ae6a81fbc8db0e29e81c206
Nida Fatima <ishaqnida7@gmail.com>
2024-07-09 17:52:18 +0500

    1

--- Full stack portfolio/vite.config.js
+++ Full stack portfolio/vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+// vite.config.js
+export default {
+  base: '/Full-Stack-portfolio/', // Ensure this matches your repository name
+  // other configurations
+};
