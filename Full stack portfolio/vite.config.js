57290747e0d02dbe217b738654db7ad365f117b0
Nida Fatima <ishaqnida7@gmail.com>
2024-07-10 00:04:59 +0500

    1

--- Full stack portfolio/vite.config.js
+++ Full stack portfolio/vite.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Full-Stack-portfolio/', 
-})
-vite.config.js
+  base: '/Full-Stack-portfolio/',
+  assetsInclude: ['**/*.jpg', '**/*.png', '**/*.jpeg', '**/*.gif', '**/*.svg'],
+});
+
